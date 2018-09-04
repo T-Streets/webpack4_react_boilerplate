555cd646f9cc9e03c5ffb26f5d35f6f9e4e83549
T-Streets <tyler.ceja5@gmail.com>
2018-09-03 23:08:58 -0700

    reconfigures webpack to handle images/font/csv/xml

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -18,6 +18,30 @@ module.exports = {
                     fallback: 'style-loader',
                     use: 'css-loader!sass-loader'
                 })
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
             }
         ]
     },
